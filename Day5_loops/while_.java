9ed18941ba2898738dd82da3f7b6cc80eb4d6d87
Gnanashriya <shriyamasula@gmail.com>
2026-05-30 23:37:54 +0530

    loops

---
+++ Day5_loops/while_.java
@@ -0,0 +1,15 @@
+package Day5_loops;
+import java.util.*;
+public class while_{
+    public static void main(String args[]){
+       int i=0;
+        while (i<11) {
+            System.out.println(i);
+            i++;   //i=i+1
+        }
+    }
+}
+// syntax:
+// while{
+//     /do some thing
+// }
