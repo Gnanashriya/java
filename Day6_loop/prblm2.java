1d26b5441ff531392c4aeb37b923edfef09781ed
Gnanashriya <shriyamasula@gmail.com>
2026-06-01 23:34:26 +0530

    problems on loops

---
+++ Day6_loop/prblm2.java
@@ -0,0 +1,13 @@
+//1-06-2026
+package Day6_loop;
+import java.util.*;
+public class prblm2 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+//        2 table
+        int num=sc.nextInt();
+        for (int i=1;i<=10;i++){
+            System.out.println(num*i);
+        } 
+    }
+}
