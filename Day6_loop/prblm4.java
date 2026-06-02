2f79100658a8588628a41247c0111d992d8ed973
Gnanashriya <shriyamasula@gmail.com>
2026-06-02 23:26:48 +0530

    problems on loops

---
+++ Day6_loop/prblm4.java
@@ -0,0 +1,26 @@
+//2-06-2026
+package Day6_loop;
+import java.util.*;
+public class prblm4 
+{
+    public static void main (String args[]){
+        Scanner sc=new Scanner(System.in);
+        int input;
+        do{
+            int marks = sc.nextInt();
+           if(marks >= 90 && marks <= 100) {
+               System.out.println("This is Good");
+           } else if(marks >= 60 && marks <= 89) {
+               System.out.println("This is also Good");
+           } else if(marks >= 0 && marks <= 59) {
+               System.out.println("This is Good as well");
+           } else {
+               System.out.println("Invalid");
+           }
+           
+           System.out.println("Want to continue ? (yes(1) or no(0))");
+           input = sc.nextInt();
+      
+       } while(input == 1);
+    }
+}
