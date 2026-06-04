7cfd7fd966c1a5aa8ca329dd4aede2698e9dc718
Gnanashriya <shriyamasula@gmail.com>
2026-06-04 23:49:31 +0530

    hallow rectangle

---
+++ Day7pattern/hallo_rect.java
@@ -0,0 +1,19 @@
+package Day7pattern;
+import java.util.*;
+public class hallo_rect {
+    public static void main(String args[]){
+        int n=4;
+        int m=7;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i==1 ||i==n|| j==1||j==m){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
